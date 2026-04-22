a2536618d2b144bf1af2258a4465a930b54b97f0
AstralCowboy <rodriguezeric1120@gmail.com>
2026-04-22 18:10:40 -0400

    1.11: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,10 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
   return (
-    <p>
-      {props.text} {props.value}
-    </p>
+    <tr>
+      <td>{props.text}</td> 
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -27,12 +28,16 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
-      <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
-      <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad)} />    
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+          <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+          <StatisticLine text="positive" value={(props.good / (props.good + props.neutral + props.bad)) * 100 + '%'} />
+        </tbody>
+      </table>
     </div>
   )
 }
